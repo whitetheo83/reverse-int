c0c09879939e8fec5c9a61f5abd341882e923fa8
Evgeni Bochkov <60480912+whitetheo83@users.noreply.github.com>
2021-08-05 15:42:39 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+//    function reverse (n) {
+
+        return (Number(String(Math.abs(n)).split("").reverse().join("")));
+
 }
+//console.log(reverse(-321));
